c6bf7b4d9b7dfa8055866c06aeada6c692c90e34
sebastian <sebastian@house-win-xp-pro>
2012-06-21 20:40:14 -0400

    first commit

---
+++ sl-myrandom-project/WebContent/js/sl-my-js-first.js
@@ -0,0 +1,10 @@
+function myadd(var1,var2)
+{
+	return var1+var2;
+}
+
+function addmult(var1,var2)
+{
+	var tot = mymult(var1,var2);
+	return tot + var1 + var2;
+}
---
+++ sl-myrandom-project/WebContent/js/sl-myrandom-0.1.js
@@ -0,0 +1,29 @@
+var sl_myrandom = function() {
+	
+	var private_var = 5;
+	
+	function private_log() {
+		console.log(private_var);
+	}
+	
+	return {
+		madd : function(var1,var2)
+		{
+			private_log();
+			return var1+var2;
+		}
+		,
+		msub : function(var1,var2)
+		{
+			return var1-var2;
+		}
+		,
+		Apple : function (type) {
+		    this.type = type;
+		    this.color = "red";
+		    this.getInfo = function() {
+		        return 'Mine is a ' + this.color + ' ' + this.type + ' apple';
+		    };
+		}
+	};
+}();
